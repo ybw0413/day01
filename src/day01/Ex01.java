0928fce0182882d63d61334e64ca4a1ffd2fd91b
byungwon <ubw4_13@naver.com>
2022-03-16 11:33:16 +0900

    Ex01 print

---
+++ src/day01/Ex01.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		
+		//괄호 안의 출력하는 기능
+		/*
+		 	여러줄 주석
+		*/
+		
+		System.out.println("안녕하세요!!");
+		System.out.println("자바시간입니다");
+
+		System.out.print("출력");
+		System.out.print("합니다");
+		
+	}
+}
