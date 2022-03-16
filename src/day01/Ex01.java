b55baf010d746246de44b12ad5154435ec5ed341
byungwon <ubw4_13@naver.com>
2022-03-16 14:00:24 +0900

    java day1

--- src/day01/Ex01.java
+++ src/day01/Ex01.java
@@ -16,5 +16,14 @@ public class Ex01 {
 		System.out.print("학원에서 추가");
 		System.out.println("집에서 추가");
 		
+		/*
+		 \n : 엔터
+		 \t : tab크기만큼 커서를 오른쪽으로 이동
+		 \" :"(큰따옴표) 표현
+		 \\ : \ 하나 표현
+		 */
+		System.out.println("안\n녕\n하\n세요");
+		System.out.println("1\t123\t12345678\t11");
+		System.out.println("  \"  \\  ");
 	}
 }
---
+++ src/day01/Ex02.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		System.out.println( 100 + 100);
+		System.out.println( 100.123 + 100);
+		
+		System.out.println("น้"+100+100);
+		System.out.println("100"+"100");
+	}
+
+}
